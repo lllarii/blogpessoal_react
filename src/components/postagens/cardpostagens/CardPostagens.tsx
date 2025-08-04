282653f7f45eac18f3bfb59ecf22670a8e0e3f6f
Lari <larissateixeira998@gmail.com>
2025-08-04 15:23:00 -0300

    adiciona lógica e componente pra deletar postagens

--- src/components/postagens/cardpostagens/CardPostagens.tsx
+++ src/components/postagens/cardpostagens/CardPostagens.tsx
@@ -36,9 +36,9 @@ function CardPostagem({ postagem }: CardPostagensProps) {
           flex items-center justify-center py-2'>
           <button>Editar</button>
         </Link>
-        <Link to=''
+        <Link to={`/deletarpostagem/${postagem.id}`}
           className='text-white bg-red-400 
-                    hover:bg-red-700 w-full flex items-center justify-center'>
+	      hover:bg-red-700 w-full flex items-center justify-center'>
           <button>Deletar</button>
         </Link>
       </div>
